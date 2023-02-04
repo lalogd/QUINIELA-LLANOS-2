a1ed56a3280170770651dd0be3f8acb42172d62a
lalogd <102998680+lalogd@users.noreply.github.com>
2023-02-04 11:32:55 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
 let res = ['_','_','_','_','_','_','_','_','_','_'];
-let quantity = localStorage.getItem('quantity');
-let name = localStorage.getItem("alias");
+
+let quantStorage = "quantity";
+let aliasStorage = "alias";
+let resultStorage = "results";
+
+
+let price = 0;  //replace *25
+let tel = "525627275572";
+let nPartidos = 10;
+let partidoSup = false;
+
+
+let quantity = localStorage.getItem(quantStorage);
+
 let combinations = false;
 var aux;
 var id = 0;
-
 
 function start(){
     recovername();
@@ -15,7 +26,7 @@ function start(){
     container.innerHTML = res.join("\xa0\xa0");
     if (quantity)
         document.querySelector('.botonenviar span').textContent = quantity;
-    let results = localStorage.getItem("results");
+    let results = localStorage.getItem(resultStorage);
     if (results){
         results = results.split("*");
         for (var i = 0; i < quantity; i++){
@@ -26,7 +37,7 @@ function start(){
                 if (results[i].split("\xa0\xa0")[0][0]!="L" && results[i].split("\xa0\xa0")[0][0]!="E" && results[i].split("\xa0\xa0")[0][0]!="V")
                 	results[i] = results[i].slice(1);
 
-			    for (var j =0; j < 10; j++) {
+			    for (var j =0; j < nPartidos; j++) {
 			    	cell1 = fila.insertCell(j)
 			    	cell1.innerHTML += results[i].split("\xa0\xa0")[j];
 			    	cell1.style.width = "7%";
@@ -36,8 +47,8 @@ function start(){
 			    		cell1.style.fontSize = "x-small";
 			    }
 
-			  	let cell2 = fila.insertCell(10);
-			  	cell2.innerHTML += "<pre>" +results[i].split("\xa0\xa0")[10] + "</pre>";
+			  	let cell2 = fila.insertCell(nPartidos);
+			  	cell2.innerHTML += "<pre>" +results[i].split("\xa0\xa0")[nPartidos] + "</pre>";
 			  	cell2.style.fontSize = "small";
 			  	cell2.style.overflow = "hidden";
 			  	cell2.style.overflowY = "hidden";
@@ -45,13 +56,13 @@ function start(){
 			  	cell2.className = "cellname";
 			  	cell2.scrollTo(80,0);
 
-			  	if(results[i].split("\xa0\xa0")[10].length > 16)
+			  	if(results[i].split("\xa0\xa0")[nPartidos].length > 15)
 			  		cell2.style.fontSize = "xx-small";
-			  	else if(results[i].split("\xa0\xa0")[10].length > 11)
+			  	else if(results[i].split("\xa0\xa0")[nPartidos].length > 10)
 			  		cell2.style.fontSize = "x-small";
 
-			  	let cell3 = fila.insertCell(10);
-			  	cell3.innerHTML += "<img src=\"img/borrar.png\" width=\"75%\">";
+			  	let cell3 = fila.insertCell(nPartidos+1);
+			  	cell3.innerHTML += '<ion-icon name="close-circle" style="color:rgb(120,0,0);"></ion-icon>';
 			  	cell3.style.width = "7%";
 			  	cell3.style.border = "none";
 			  	cell3.id = "x" + i;
@@ -62,10 +73,12 @@ function start(){
                 break;
         }
         id = i;
-    document.getElementById("total").innerHTML = "Total: $" + quantity*0 +"\n";
+    document.getElementById("total").innerHTML = "Total: $" + quantity*price +"\n";
 	}  
 }
+
 function updatedisplay(modo){
+
     let display = document.getElementById("display");
     let lastIndex = display.getElementsByTagName("tr").length;
 
@@ -75,7 +88,7 @@ function updatedisplay(modo){
         
         let fila = display.insertRow(lastIndex);
 
-        for (var j =0; j < 10; j++) {
+        for (var j =0; j < nPartidos; j++) {
         	cell1 = fila.insertCell(j)
         	cell1.innerHTML += res[j];
         	cell1.style.width = "7%";
@@ -86,10 +99,10 @@ function updatedisplay(modo){
         }
 
         if (aux>1){
-    		var cellname = fila.insertCell(10);
+    		var cellname = fila.insertCell(nPartidos);
     		cellname.innerHTML += name + " (" + aux + ")";}
     	else{
-    		var cellname = fila.insertCell(10);
+    		var cellname = fila.insertCell(nPartidos);
     		cellname.innerHTML += name;}
 
       	cellname.style.fontSize = "small";
@@ -99,13 +112,13 @@ function updatedisplay(modo){
       	cellname.className = "cellname";
       	cellname.scrollTo(80,0);
 
-    	if(name.length > 16)
+    	if(name.length > 15)
       		cellname.style.fontSize = "xx-small";
       	else if(name.length > 10)
       		cellname.style.fontSize = "x-small";
 
-    	let cell3 = fila.insertCell(10);
-      	cell3.innerHTML += "<img src=\"img/borrar.png\" width=\"75%\">";
+    	let cell3 = fila.insertCell(nPartidos+1);
+      	cell3.innerHTML += '<ion-icon name="close-circle" style="color:rgb(120,0,0);"></ion-icon>';
       	cell3.style.width = "7%";
       	cell3.style.border = "none";
       	cell3.id = "x" + id;
@@ -113,6 +126,7 @@ function updatedisplay(modo){
       	cell3.addEventListener('click', function(){remove(this); this.replaceWith(this.cloneNode(true));});
       	id++;
         aux = 1;
+        //cell1.innerHTML += res.join("\xa0\xa0") + "\xa0\xa0" + name + " (" + aux + ")" + "*\xa0\xa0";
     }
     else if (modo == 2){
         id = 0;
@@ -121,10 +135,12 @@ function updatedisplay(modo){
             deleters[i].id = "x" + id;
             id++;
         }
+
     }
-    document.getElementById("total").innerHTML = "Total: $" + quantity*0;
+    document.getElementById("total").innerHTML = "Total: $" + quantity*price;
 }
-function selection(element){ 	//Pinta la casilla y actualiza el texto de la quiniela.
+
+function selection(element){ 		//Pinta la casilla y actualiza el texto de la quiniela.
     let index = parseInt(element.id.slice(1)) - 1;
     let container = document.getElementById("text");
     if (!element.style.backgroundColor){
@@ -136,7 +152,7 @@ function selection(element){ 	//Pinta la casilla y actualiza el texto de la quin
             res[index] += element.id.slice(0,1);
             res[index] = res[index].split('_').join('');
         }
-        element.style.backgroundColor = "#080808";
+        element.style.backgroundColor = "rgb(65, 65, 67)";
     }
     else{
         if(combinations){
@@ -151,40 +167,44 @@ function selection(element){ 	//Pinta la casilla y actualiza el texto de la quin
     costoactual();
     container.innerHTML = res.join("\xa0\xa0");
 }
+
 function validation(element){	 //Despinta todas las casillas y asigna el index con el número de la casilla seleccionada.
     let index = parseInt(element.id.slice(1));
     document.getElementById("L"+index).style.backgroundColor = null;
     document.getElementById("E"+index).style.backgroundColor = null;
     document.getElementById("V"+index).style.backgroundColor = null;
 }
+
 function number(){ 				//Actualiza el número del boton "Enviar"
-    quantity = localStorage.getItem("quantity");
+    quantity = localStorage.getItem(quantStorage);
     if (quantity)
-        localStorage.setItem('quantity', ++quantity);
+        localStorage.setItem(quantStorage, ++quantity);
     else{
-        localStorage.setItem('quantity', 1);
-        quantity = localStorage.getItem("quantity");}
+        localStorage.setItem(quantStorage, 1);
+        quantity = localStorage.getItem(quantStorage);}
     document.querySelector('.botonenviar span').textContent = quantity;
-    localStorage.setItem('alias', name);
+    localStorage.setItem(aliasStorage, name);
 }
-function result(){ 	//Actualiza el localstorage cuando se añade una nueva quiniela
-    results = localStorage.getItem("results");
+
+function result(){ 				//Actualiza el localstorage cuando se añade una nueva quiniela
+    results = localStorage.getItem(resultStorage);
     name = document.getElementById("nombre").value;
     name  = name.split('*').join('');
     if (results){
         if (aux > 1)
-        localStorage.setItem('results', results + "\n" + res.join("\xa0\xa0") + "\xa0\xa0" + name + " (" + aux + ")" + "*");
+        localStorage.setItem(resultStorage, results + "\n" + res.join("\xa0\xa0") + "\xa0\xa0" + name + " (" + aux + ")" + "*");
         else
-            localStorage.setItem('results', results + "\n" + res.join("\xa0\xa0") + "\xa0\xa0" + name + "*");
+            localStorage.setItem(resultStorage, results + "\n" + res.join("\xa0\xa0") + "\xa0\xa0" + name + "*");
     }
     else
     {
         if (aux > 1)
-        localStorage.setItem('results',res.join("\xa0\xa0") + "\xa0\xa0" + name + " (" + aux + ")" +  "*");
+        localStorage.setItem(resultStorage,res.join("\xa0\xa0") + "\xa0\xa0" + name + " (" + aux + ")" +  "*");
         else    
-            localStorage.setItem('results',res.join("\xa0\xa0") + "\xa0\xa0" + name+ "*");
+            localStorage.setItem(resultStorage,res.join("\xa0\xa0") + "\xa0\xa0" + name+ "*");
     }       
 }
+
 function save(){				//Se añade la quiniela actual a la lista 
 	if(id < 150){
 	    name = document.getElementById("nombre").value;
@@ -198,34 +218,38 @@ function save(){				//Se añade la quiniela actual a la lista
 	        if (combinations)
 	            calculate();
 	        else
-                number();
-                result();
-                updatedisplay(1);
-                clean();
-            }
+	            number();
+	        result();
+	        updatedisplay(1);
+	        clean();}
     }
     else
-    	alert("Envía tus quinielas registradas antes de capturar más.");
+    	alert("Envía tus quinielas registradas antes de capturar más");
 }
+
 function clean(){				//Boton para limpiar la quinela
-    res = ['_','_','_','_','_','_','_','_','_']
+    res = ['_','_','_','_','_','_','_','_','_','_']
     let container = document.getElementById("text");
     container.innerHTML = res.join("\xa0\xa0");
     spans = document.querySelectorAll(".quiniela span");
-    for (var i=0; i<30;i++)
+    for (var i=0; i<nPartidos*3;i++)
         spans[i].style.backgroundColor = "";
     document.getElementById("costo").innerHTML = "Costo: $0";
     document.getElementById("numquinielas").innerHTML = "0 Quiniela(s)";
 }
+
 //UPDATEDISPLAY
+
 function recovername(){
-    name = localStorage.getItem("alias");
+    name = localStorage.getItem(aliasStorage);
     if (name !=  null && name !="null")
         document.getElementById("nombre").value = name;
 }
+
 function clearname(){
     document.getElementById("nombre").value = "";
 }
+
 function allowcombination(){
     if (!combinations) 
     {
@@ -243,70 +267,85 @@ function allowcombination(){
         clean();
     }
 }
+
 function calculate(){
     aux = 1;
-    for (var i=0;i<10;i++){
+    for (var i=0;i<nPartidos;i++){
         aux*= res[i].length;
     }
-    quantity = localStorage.getItem("quantity");
+    quantity = localStorage.getItem(quantStorage);
     if (quantity){
-        localStorage.setItem('quantity', parseInt(quantity)+aux);
-        quantity = localStorage.getItem('quantity');}
+        localStorage.setItem(quantStorage, parseInt(quantity)+aux);
+        quantity = localStorage.getItem(quantStorage);}
     else{
-        localStorage.setItem('quantity', aux);
-        quantity = localStorage.getItem("quantity");}
+        localStorage.setItem(quantStorage, aux);
+        quantity = localStorage.getItem(quantStorage);}
     document.querySelector('.botonenviar span').textContent = quantity;
-    localStorage.setItem('alias', name);
+    localStorage.setItem(aliasStorage, name);
 }
+
 function random(){
     clean();
     let container = document.getElementById("text");
     let partidos = document.getElementsByClassName("partido");
-    for (var i = 0; i < 10; i++){
+    for (var i = 0; i < nPartidos; i++){
         var r = getRandomInt(0,2);
-        partidos[i].getElementsByTagName("span")[r].style.backgroundColor = "#080808";
+        partidos[i].getElementsByTagName("span")[r].style.backgroundColor = "rgb(65, 65, 67)";
         res[i] = ["L","E","V"][r];
     }
     container.innerHTML = res.join("\xa0\xa0");
     costoactual();
 }
+
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
 function costoactual(){
     if (true){
         let aux2 = 1;
-        for (var i=0;i<10;i++){
+        for (var i=0;i<nPartidos;i++){
              aux2*= res[i].length;
         }
-        document.getElementById("costo").innerHTML = "Costo: $" + aux2*0;
+        document.getElementById("costo").innerHTML = "Costo: $" + aux2*price;
         document.getElementById("numquinielas").innerHTML = aux2 + " Quiniela(s)"
     }
 }
 function remove(e){
 	display = document.getElementById("display");
+	//console.log(e.id.slice(1));
+	//console.log(display.getElementsByTagName("tr"));
 	eindex = e.id.slice(1);
 	tr = display.getElementsByTagName("tr")[eindex];
+
 	display.deleteRow(eindex);
-    results = localStorage.getItem("results");
+
+
+    results = localStorage.getItem(resultStorage);
     results = results.split("*");
+
 	removing = results[eindex].split("\xa0\xa0");
+	//console.log("removing",removing);
 	if (removing[0][0]!="L" && removing[0][0]!="E" && removing[0][0]!="V")
         removing[0] = removing[0].slice(1);
 	let aux3 = 1;
-    for (var i=0;i<10;i++)
+    for (var i=0;i<nPartidos;i++)
         aux3*= removing[i].length;
     quantity -= aux3;
-    localStorage.setItem("quantity",quantity);
+    localStorage.setItem(quantStorage,quantity);
 	results.splice(eindex,1);
+    //console.log(results);
     results = results.join("*");
-    localStorage.setItem("results",results);
+    localStorage.setItem(resultStorage,results);
+
     document.querySelector('.botonenviar span').textContent = quantity;
-    document.getElementById("total").innerHTML = "Total: $" + quantity*0 +"\n";
+    document.getElementById("total").innerHTML = "Total: $" + quantity*price +"\n";
     updatedisplay(2);
+
 }
+
 function removeLastLine(){
     if(resultsx.lastIndexOf("\n")>0) {
         return resultsx.substring(0, resultsx.lastIndexOf("\n"));
@@ -314,21 +353,24 @@ function removeLastLine(){
         return resultsx;
     }
 }
+
 function deleteall(){
-    if(confirm("Se borrará todo."))
-        {localStorage.setItem("quantity","");
-        localStorage.setItem("results","");
-        localStorage.setItem("alias","");
+    if(confirm("Se borrará todo"))
+        {localStorage.setItem(quantStorage,"");
+        localStorage.setItem(resultStorage,"");
+        //localStorage.setItem(aliasStorage,"");
         location.reload();}
 }
-function send(){ //Envía quiniela al whatsapp
+
+function send(){				//Envia la quiniela al whatsapp 
     if (!quantity || quantity < 1)
         save();
     if (quantity > 0){
     let whatsapptext = res.join("%20%20")
     const quinbara = "*Quiniela*%0A%0A";
-    whatsapptext = encodeURI(localStorage.getItem("results"));
     whatsapptext = whatsapptext.split('*').join('%0D').replace(/#/g,"");
-    window.location.href = "https://wa.me/525627275572?text="+quinbara + whatsapptext;}
+    whatsapptext = encodeURI(localStorage.getItem(resultStorage));
+    window.location.href = "https://wa.me/"+tel+"?text="+quinbara+whatsapptext;}
 }
+
 window.addEventListener("load",start,false);
